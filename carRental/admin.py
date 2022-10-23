3f4d0ecd20bd2b8a6c2a174da6671035e55d4009
GE3-CPK <60844728+GE3-CPK@users.noreply.github.com>
2022-10-23 15:01:32 +0300

    h

--- carRental/admin.py
+++ carRental/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from . models import RentalCompany, Car, Manufacturer
+from . models import RentalCompany, Car, Manufacturer, Costumer, PlaceToStart, Rental
 
 admin.site.register(RentalCompany)
 admin.site.register(Car)
 admin.site.register(Manufacturer)
-
-
+admin.site.register(Costumer)
+admin.site.register(PlaceToStart)
+admin.site.register(Rental)
--- carRental/models.py
+++ carRental/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 TYPE_CHOICES = [('MINI', 'mini'), ('Medium', 'medium'), ('MID_RANGE', 'mid-range'), ('SEDAN', 'sedan'), ('SUV', 'suv')]
-TRANSMISSION_CHOICES = [('MANUAL', 'manual'), ('AUTOMATIC', 'automatic'), ]
+TRANSMISSION_CHOICES = [('MANUAL', 'manual'), ('AUTOMATIC', 'automatic')]
+PLACE_CHOICES = [('Home', 'home'), ('Airport', 'airport'), ('KTEL', 'ktel')]
 
 
 class RentalCompany (models.Model):
@@ -11,23 +12,43 @@ class RentalCompany (models.Model):
     companyPhoneNumber = PhoneNumberField(null=False, blank=False, unique=True)
     companyEmail = models.EmailField(max_length=254, null=False, default='')
     owner = models.ForeignKey('auth.User', related_name='carRental', on_delete=models.CASCADE, null=True)
-    car = models.ForeignKey('Car', null=True, on_delete=models.CASCADE)
+    costumer = models.ManyToManyField('Costumer')
+    car = models.ManyToManyField('Car')
+    placeToStart = models.ManyToManyField('PlaceToStart')
     companyLogo = models.ImageField(null=True)
 
     def __str__(self):
         return self.companyName
 
 
+class Costumer(models.Model):
+    costumerFirstName = models.CharField(max_length=80, null=False, default='')
+    costumerLastName = models.CharField(max_length=80, null=False, default='')
+    costumerEmail = models.EmailField(max_length=254, null=False, default='')
+    costumerPhoneNumber = PhoneNumberField(null=False, blank=False, unique=True)
+    costumerAFM = models.IntegerField(null=False, default='')
+
+    def __str__(self):
+        return self.costumerFirstName
+
+
 class Car (models.Model):
     carModel = models.CharField(max_length=100, blank=False, default='')
     manufacturer = models.ForeignKey('Manufacturer', null=True, on_delete=models.CASCADE)
-    type = models.CharField(choices=TYPE_CHOICES, default='', max_length=100)
-    transmission = models.CharField(choices=TRANSMISSION_CHOICES, default='', max_length=100)
+    type = models.CharField(choices=TYPE_CHOICES, default='', max_length=15)
+    transmission = models.CharField(choices=TRANSMISSION_CHOICES, default='', max_length=15)
     carImage = models.ImageField(null=True)
     owner = models.ForeignKey('auth.User', related_name='car', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.carModel
+
+
+class PlaceToStart(models.Model):
+    placeToStart = models.CharField(max_length=15, choices=PLACE_CHOICES)
+
+    def __str__(self):
+        return self.placeToStart
 
 
 class Manufacturer(models.Model):
@@ -38,3 +59,14 @@ class Manufacturer(models.Model):
     def __str__(self):
         return self.manufacturerName
 
+
+class Rental(models.Model):
+    costumer = models.ForeignKey('Costumer', null=True, on_delete=models.CASCADE)
+    rentalCompany = models.ForeignKey('RentalCompany', null=True, on_delete=models.CASCADE)
+    car = models.ForeignKey('Car', null=True, on_delete=models.CASCADE)
+    startDate = models.DateField()
+    finishDate = models.DateField()
+    placeToStart = models.ForeignKey('PlaceToStart', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.costumer
